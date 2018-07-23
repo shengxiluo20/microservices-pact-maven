7196d27b61f54aeed638a6a539c5e37723199d3e
shengxiluo <chi18765928341@163.com>
2018-07-23 19:39:19 +0800

    添加多接口调用

--- microservices-pact-consumer/src/main/java/io/pivotal/microservices/pact/consumer/ProviderHandler.java
+++ microservices-pact-consumer/src/main/java/io/pivotal/microservices/pact/consumer/ProviderHandler.java
@@ -1,52 +1,49 @@
 package io.pivotal.microservices.pact.consumer;
 
-import au.com.dius.pact.model.Request;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ProviderHandler {
 
-    private String backendURL = "http://localhost:8080/information?name=Miku";
-
-    public String getBackendURL() {
-        return this.backendURL;
-    }
-
-    public void setBackendURL(String URLBase) {
-        this.backendURL = URLBase+"/information?name=Miku";
-    }
-    public void setBackendURL(String URLBase, String name) {
-        this.backendURL = URLBase+"/information?name="+name;
-    }
-
-    public Information getInformation() {
-        RestTemplate restTemplate = new RestTemplate();
-        Information information = restTemplate.getForObject(getBackendURL(), Information.class);
-
-        return information;
-    }
 
 
 
-
-    /*private String backendURL;
+    private String backendURL;
 
     public ProviderHandler(String backendURL) {
         this.backendURL = backendURL;
     }
 
-    public void getInformation(Request request) {
+
+    public void getInformation(String path, Object body, String method, Map<String, List<String>> query) {
+
         RestTemplate restTemplate = new RestTemplate();
         ParameterizedTypeReference<String> responseType = new ParameterizedTypeReference<String>() {
         };
 
+        HashMap<String, String> map = new HashMap<>();
+        for (String s : query.keySet()) {
+            try {
+                map.put(s,query.get(s).get(0));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.parseMediaType("application/json; charset=UTF-8"));
         headers.add("Accept", MediaType.APPLICATION_JSON.toString());
-        HttpEntity<String> entity = new HttpEntity<>(JSONObject.toJSONString(request.getBody()), headers);
-        ResponseEntity<String> exchange = restTemplate.exchange(backendURL + "/" + request.getPath(), HttpMethod.resolve(request.getMethod()), entity, responseType, request.getQuery());
+        HttpEntity<String> entity = new HttpEntity<>(JSONObject.toJSONString(body), headers);
+
+        //TODO chi
+        ResponseEntity<String> exchange = restTemplate.exchange(backendURL + "/" + path+"?name="+map.get("name"), HttpMethod.resolve(method), entity, responseType, map);
         System.out.println("===================" + exchange.getBody());
-    }*/
+    }
 }
--- microservices-pact-consumer/src/test/java/io/pivotal/microservices/pact/consumer/PactJunitRuleMultipleInteractionsTest.java
+++ microservices-pact-consumer/src/test/java/io/pivotal/microservices/pact/consumer/PactJunitRuleMultipleInteractionsTest.java
@@ -4,11 +4,14 @@ import au.com.dius.pact.consumer.Pact;
 import au.com.dius.pact.consumer.PactProviderRuleMk2;
 import au.com.dius.pact.consumer.PactVerification;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.Request;
+import au.com.dius.pact.model.RequestResponseInteraction;
 import au.com.dius.pact.model.RequestResponsePact;
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class PactJunitRuleMultipleInteractionsTest {
@@ -16,12 +19,14 @@ public class PactJunitRuleMultipleInteractionsTest {
     @Rule
     public PactProviderRuleMk2 mockProvider = new PactProviderRuleMk2("Foo_Provider",this);
 
+    private RequestResponsePact pact;
+
     @Pact(provider="Foo_Provider", consumer="Foo_Consumer")
     public RequestResponsePact createPact(PactDslWithProvider builder) {
         Map<String, String> headers = new HashMap<String, String>();
         headers.put("Content-Type", "application/json;charset=UTF-8");
 
-        return builder
+        RequestResponsePact pact = builder
                 .given("")
                 .uponReceiving("Miku")
                 .path("/information")
@@ -57,29 +62,22 @@ public class PactJunitRuleMultipleInteractionsTest {
                         "    }\n" +
                         "}")
                 .toPact();
+
+        this.pact = pact;
+
+        return pact;
     }
 
     @Test
     @PactVerification("Foo_Provider")
     public void runTest() {
-        ProviderHandler providerHandler = new ProviderHandler();
+        ProviderHandler providerHandler = new ProviderHandler(mockProvider.getUrl());
 
-
-        String url = mockProvider.getUrl();
-        String url1 = mockProvider.getUrl();
-
-
-        providerHandler.setBackendURL(mockProvider.getUrl());
-        Information information = providerHandler.getInformation();
-
-        providerHandler.setBackendURL(mockProvider.getUrl(), "Nanoha");
-        information = providerHandler.getInformation();
-
-
-        /*List<RequestResponseInteraction> interactions = pacts.getInteractions();
+        List<RequestResponseInteraction> interactions = pact.getInteractions();
 
         for (RequestResponseInteraction interaction : interactions) {
-            providerHandler.getInformation(interaction.getRequest());
-        }*/
+            Request request = interaction.getRequest();
+            providerHandler.getInformation(request.getPath(),request.getBody(),request.getMethod(),request.getQuery());
+        }
     }
 }
